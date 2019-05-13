5e94216709c654f6ccc415823482e3d964c1aabe
Kaioru <keifuurawrs@gmail.com>
2019-05-13 15:22:37 +0800

    Add elevator script

---
+++ src/elevator.lua
@@ -0,0 +1,7 @@
+contimove = target.get_continent()
+
+if contimove.state == 0x1 then
+    target.field = contimove.wait_field
+else
+    target.message("The elevator is not available for the riding at this time. Please try againt later.")
+end
