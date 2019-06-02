e2c265f4812f8e043aadb0425a95f3cc35a11532
Bia10 <t.locke1234@gmail.com>
2019-06-02 18:24:56 +0200

    [Add] bunch of begining maps scripts

---
+++ src/in2159011.lua
@@ -0,0 +1,8 @@
+speaker = self.as_speaker(2159011)
+
+if speaker.ask_yes_no("#b(What a suspicious hole. Maybe Von is hiding inside. Peek inside?)#k") then
+  target.exp = target.exp + 35
+  target.field = 931000010
+else
+  return
+end
