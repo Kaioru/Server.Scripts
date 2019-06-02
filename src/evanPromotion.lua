7f89333063cadfcc5b62618433deb3943aefb50c
Kaioru <keifuurawrs@gmail.com>
2019-06-02 09:17:03 +0800

    Fix evanPromotion

--- src/evanPromotion.lua
+++ src/evanPromotion.lua
@@ -1,13 +1,12 @@
---todo: disable UI,lock actions, enable actions
-
+-- TODO: directionmode/lockUI
 if target.field == 900090000 then
-    target.squib_effect("Effect/Direction4.img/promotion/Scene0" .. target.gender)
+    target.reserved_effect("Effect/Direction4.img/promotion/Scene0" .. target.gender)
 elseif target.field == 900090001 then
-    target.squib_effect("Effect/Direction4.img/promotion/Scene1")
+    target.reserved_effect("Effect/Direction4.img/promotion/Scene1")
 elseif target.field == 900090002 then
-    target.squib_effect("Effect/Direction4.img/promotion/Scene2" .. target.gender)
+    target.reserved_effect("Effect/Direction4.img/promotion/Scene2" .. target.gender)
 elseif target.field == 900090003 then
-    target.squib_effect("Effect/Direction4.img/promotion/Scene3")
+    target.reserved_effect("Effect/Direction4.img/promotion/Scene3")
 elseif target.field == 900090004 then
-	target.transfer_field(900010000, 0)
+	target.field = 900010000
 end
