be20e390880618dd9fb252d70b4606515c87b5ec
Bia10 <t.locke1234@gmail.com>
2019-06-03 04:03:07 +0200

    Fix hide&seek scripts

--- src/talk2159015.lua
+++ src/talk2159015.lua
@@ -1,9 +1,9 @@
 quest = target.get_quest(23999)
 
-if quest.record_key("exp3") == "1"  then
-self.say("Hehehe...") 
+if quest.record_key("exp3") == "1" then
+	self.say("Hehehe...") 
+else
+	self.say("Aw shucks. You found me. Wow, you're really good at this game!\r\n\r\n#fUI/UIWindow.img/QuestIcon/8/0# 3 exp")
+	target.exp = target.exp + 3
+	quest.update("exp3", "1")
 end
-
-self.say("Aw shucks. You found me. Wow, you're really good at this game!\r\n\r\n#fUI/UIWindow.img/QuestIcon/8/0# 3 exp")
-target.exp = target.exp + 3
-quest.update("exp3", "1")
--- src/talk2159016.lua
+++ src/talk2159016.lua
@@ -1,9 +1,9 @@
 quest = target.get_quest(23999)
 
-if quest.record_key("exp4") == "1"  then
-self.say("Hehehe...") 
+if quest.record_key("exp4") == "1" then
+	self.say("Hehehe...") 
+else
+	self.say("D'oh! You found me. But I'm tiny! Are you a professional at this game or something?\r\n\r\n#fUI/UIWindow.img/QuestIcon/8/0# 3 exp")
+	target.exp = target.exp + 3
+	quest.update("exp4", "1")
 end
-
-self.say("D'oh! You found me. But I'm tiny! Are you a professional at this game or something?\r\n\r\n#fUI/UIWindow.img/QuestIcon/8/0# 3 exp")
-target.exp = target.exp + 3
-quest.update("exp4", "1")
