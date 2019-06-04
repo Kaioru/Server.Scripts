a81a8186e88a59ee92f865a06579b657bac01f99
Bia10 <t.locke1234@gmail.com>
2019-06-04 13:18:52 +0200

    [Add] q1021s [Clean] go20000

--- src/go20000.lua
+++ src/go20000.lua
@@ -1,14 +1,1 @@
 target.screen_field_effect("maplemap/enter/20000")
-
---todo: quests
---[[quest1 = target.get_quest(32200)
-quest1.accept()
-quest1.complete()
-
-quest2 = target.get_quest(32201)
-quest2.accept()
-quest2.complete()
-
-quest3 = target.get_quest(32202)
-quest3.accept()
-quest3.complete()]]--
