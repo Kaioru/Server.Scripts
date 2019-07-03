df35ac04b6e8f83a479e741cc477a9d9304f918d
Keith <keifuurawrs@gmail.com>
2019-07-03 11:14:44 +0800

    Fix typo

--- src/elevator.lua
+++ src/elevator.lua
@@ -3,5 +3,5 @@ contimove = target.get_continent()
 if contimove.state == 0x1 then
     target.field = contimove.wait_field
 else
-    target.message("The elevator is not available for the riding at this time. Please try againt later.")
+    target.message("The elevator is not available for the riding at this time. Please try again later.")
 end
