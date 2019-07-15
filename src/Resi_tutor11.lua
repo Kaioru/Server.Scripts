318cf765d717a7bb9ebc6e71760cbe013675abae
Bia10 <t.locke1234@gmail.com>
2019-07-15 13:56:48 +0200

    typo quickfix

--- src/Resi_tutor11.lua
+++ src/Resi_tutor11.lua
@@ -12,14 +12,14 @@ self.say({
     target.get_speech("Don't be ridiculous."),
     
     jun.get_speech("You're not s-s-scared at all? I am, a little b-b-bit... The grown-ups warned us never to venture into the #bVerne Mines#k... Plus, there are all those #rBlack Wings#k around, watching us, I just know it."),
-    von.get_speech("We snuck here, jun. No one saw us. No one's watching us, okay? Come on, when else would we have ever gotten the chance to leave #bEdelstein#k? Don't be a chicken."),
+    von.get_speech("We snuck here, Jun. No one saw us. No one's watching us, okay? Come on, when else would we have ever gotten the chance to leave #bEdelstein#k? Don't be a chicken."),
     jun.get_speech("But what if we get in trouble?"),
-    ulrika.get_speech("jun, we're already here. If we're going to get in trouble, let's at least have some fun first. Let's play hide-and-seek!"),
+    ulrika.get_speech("Jun, we're already here. If we're going to get in trouble, let's at least have some fun first. Let's play hide-and-seek!"),
     
     target.get_speech("Hide and seek?"),
     
     von.get_speech("Ugh, la-ame."),
-    ulrika.get_speech("Don't be a brat, von. What? Are you scared to hide all by yourself in these big, bad caves? *snicker* \r\n#h0#, since you were late, you're it. Count to 10 and then come find us. No peeking.")
+    ulrika.get_speech("Don't be a brat, Von. What? Are you scared to hide all by yourself in these big, bad caves? *snicker* \r\n#h0#, since you were late, you're it. Count to 10 and then come find us. No peeking.")
 })
 
 target.transfer_field(mapFields.getID("NeglectedRockyMountain2"), 1)
